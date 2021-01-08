82855bc53b82a22ce60e08816915e0530a487e3a
Miunmn <esteban.principe@utec.edu.pe>
2021-01-08 16:03:36 -0500

    16:03

--- CSES/main.cpp
+++ CSES/main.cpp
@@ -1,17 +1,39 @@
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
-
-int main(){
+int main()
+{
     using ll = long long int;
-    ll t;
-    cin>>t;
-    while(t--){
-        ll a,b;
-        cin>>a>>b;
-        cout<<"a: "<<a<<" b:"<<b<<"\n";
-        string output = (a+b)%3 == 0 && 2*a>=b && 2*b>=a?"YES":"NO"    ;
-        cout<<output<<"\n";
+    /*
+    Given a string, your task is to reorder
+    its letters in such a way that it becomes a
+    palindrome (i.e., it reads 
+    the same forwards and backwards).
+    */
+    string s; 
+    cin>>s;
+    int c[26] = {}, c1=0;
+    for(char d : s)
+        ++c[d-'A'];
+    for(int i = 0 ; i < 26 ; ++i)
+        c1+=c[i]&1;
+    if(c1>1){
+        cout<<"NO SOLUTION";
+    return 0;
     }
-return 0;
+    string t;
+    for(int i = 0 ; i < 26; ++i)
+        if(c[i]&1^1)
+            for(int j = 0 ; j < c[i]/2; ++j)
+                t+=(char)('A'+i);    
+    cout<<t;
+    for(int i = 0 ; i < 26 ; i++)
+        if(c[i]&1)
+            for(int j = 0 ; j<c[i]; j++)
+                cout<<(char)('A'+i);
+
+    reverse(t.begin(),  t.end());
+    cout<<t;
+    return 0;
 }
